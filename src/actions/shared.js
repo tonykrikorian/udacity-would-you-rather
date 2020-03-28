b0682abd63d2ec80d3901c321a01119e34b64c03
Tony <tony.krikorian@outlook.com>
2020-03-28 11:49:13 -0300

    install axios

---
+++ src/actions/shared.js
@@ -0,0 +1,10 @@
+import { RECEIVE_DATA } from "../types/sharedTypes";
+import axios from "axios";
+function receiveData(posts) {
+  return {
+    type: RECEIVE_DATA,
+    posts
+  };
+}
+
+export function handleInitialData() {}
