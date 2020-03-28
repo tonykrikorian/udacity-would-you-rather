c781afdc490b3bd3beceff4215acb27d54f6fa6c
Tony <tony.krikorian@outlook.com>
2020-03-28 11:36:42 -0300

    fix middleware

--- src/middleware/index.js
+++ src/middleware/index.js
@@ -2,4 +2,4 @@ import { applyMiddleware } from "redux";
 import logger from "./logger";
 import thunk from "redux-thunk";
 
-export default applyMiddleware({ thunk, logger });
+export default applyMiddleware(thunk, logger);
